40531c203130c52e158a9e487736559fe137224c
Robert <jankowski.r@hotmail.com>
2022-09-19 19:52:52 +0200

    Empty argument and one argument

--- src/main/java/org/example/StringCalc.java
+++ src/main/java/org/example/StringCalc.java
@@ -1,9 +1,26 @@
 package org.example;
 
+import java.util.stream.Stream;
+
 public class StringCalc {
+    //Create a simple String calculator with a method int Add(string numbers)
 
-    public static void add() {
+    /**
+     *
+     * @param args
+     * @return
+     */
+    public static int add(String numbers) {
+        if(hasBlankArgs(numbers)) {
+            return 0;
+        }
 
+        return Integer.parseInt(numbers);
+    }
+
+
+    private static boolean hasBlankArgs(String args) {
+        return args == null || args.isEmpty();
     }
 
 }
